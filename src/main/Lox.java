4c7fa243b625faaa39c32e00628088a394e61b88
Beehive324 <Fairson123@hotmail.com>
2023-08-09 22:55:36 +0100

    Error Handling + run method

--- src/main/Lox.java
+++ src/main/Lox.java
@@ -1,7 +1,10 @@
 package main;
 
-
+//executes from souce
 public class Lox {
+
+    static boolean hadError = False;
+
     public static void main(String[] args) throws Exception {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]"); 
@@ -11,6 +14,7 @@ public class Lox {
         }else{
             runPrompt(); // call the method runPrompt
         }
+        if hadError System.exit(65);
         }
     
         // Read a line of input, evaluate it, print the result and then loop and then repear the process again
@@ -23,9 +27,33 @@ public class Lox {
                 String line = reader.readLine(); // readline function, reads a line of input from the user on the command line and returns the result
                 if (line == null) break;
                 run(line);
+                hadError = False;
             }
         }
         
         
-    }
+        //Run method
+        private static void run(String source) {
+            Scanner scanner = new Scanner(source); //reading the input from the string
+            List<Token> tokens = scanner.scanTokens() // reading from the token class
+
+            for (Token token : tokens) { //Prints out the tokens
+                System.out.println(token);
+            }
+        }
+        
+        //Error handling
+        static void error(int line, message) {
+            report(line, "", message;)
+        }
+
+        private static void(int line, String where, String message) {
+            System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }      
+        
+
+        }
+    
 
