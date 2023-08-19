dab8bb69479cd694517f7277e208f6c138a37d79
Fairson <fs00602@surrey.ac.uk>
2023-08-19 21:17:19 +0100

    Scanner class

--- src/main/Scanner.java
+++ src/main/Scanner.java
@@ -41,7 +41,76 @@ class Scanner {
 }
 
     private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break; 
+            //Operators
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')){
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }else{
+                    addToken(SLASH)''
+                }
+                }
+             default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
         
     }
 
-}
+
+    // advance method consumes the next character in the source file and returns it
+    //Advance is for input
+    private char advance() {
+
+        return source.charAt(current++);
+
+    }
+    //Grabs the text of the current lexeme and creates a new token for it 
+    private void addToken(TokenType TokenType){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Obejct literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if(isAtEnd()) return false;
+        if(souce.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+
+
+    }
+
+    //Looks at the currenct unconsumed character
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
